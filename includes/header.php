46124327e9c174d13deb0dfab9b97de968258b84
Luiz Guilherme <luiz1223@hotmail.com>
2019-09-11 14:45:07 -0300

    Initial commit.

---
+++ includes/header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title></title>
+    <meta name="description" content="">
+    <meta name="language" content="pt-br">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, user-scalable=no" />
+    <meta name="apple-mobile-web-app-capable" content="yes" />
+    <meta name="format-detection" content="telephone=no">
+    <meta name="author" content="">
+    <meta name="language" content="pt-br" />
+    <link rel="canonical" href="<?= $_SERVER['SERVER_NAME'].$_SERVER['REQUEST_URI'] ?>" />
+</head>
+<body>
+    
